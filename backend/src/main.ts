9c22ad443293666d8f73b20927d14224ced673e0
husbanraina <mohammadhusbanraina@gmail.com>
2024-03-06 13:36:01 +0530

    add log for server listening

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const PORT = 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  console.log(`Server is listening on port : ${PORT}`)
 }
 bootstrap();
