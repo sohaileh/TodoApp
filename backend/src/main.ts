2b13cca43bd65da3eab0d099f3516b12c58ceb57
husbanraina <mohammadhusbanraina@gmail.com>
2024-03-12 17:47:40 +0530

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,11 @@ const cookieSession = require('cookie-session')
 const PORT = 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.use(cookieSession({
     keys: ['myKey123']
   }))
